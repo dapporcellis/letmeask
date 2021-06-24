12478bdc1faa5c1d6ffee386855fe60d2831ee24
Diego de Abreu Porcellis <dapporcellis@gmail.com>
2021-06-24 19:30:36 -0300

    Aula 4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,6 +31,11 @@ export function Home() {
             alert('Room does not exists')
             return;
         }
+        if(roomRef.val().endAt){
+            alert('Room already closed.')
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`)
     }
 
